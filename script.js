c1e87f9dc77eaf902dab8c1514fa012571d101e8
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-28 20:03:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-window.promises = [];
+ const promises = [
+      new Promise(resolve => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 4000 + 1000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 4000 + 1000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 4000 + 1000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 4000 + 1000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 4000 + 1000))
+    ];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+    // Print the result of the first resolved promise to the output div
+    Promise.any(promises)
+      .then(result => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = result;
+      })
+      .catch(error => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = "All promises failed: " + error.message;
+      });
