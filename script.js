fe14d0ad5b02c3fe533bc424012e2daeff0786f7
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-29 10:52:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
- const promises = [
-      new Promise((resolve,reject)=>{
-		  setTimeout(()=>{
-			  resolve("first array");
-		  },1000)
-	  })
-	  new Promise((resolve,reject)=>{
-		  setTimeout(()=>{
-			  resolve("second array");
-		  },2000)
-	  })
-	  new Promise((resolve,reject)=>{
-		  setTimeout(()=>{
-			  resolve("third array");
-		  },3000)
-	  })
-	 new Promise((resolve,reject)=>{
-		  setTimeout(()=>{
-			  resolve("fourth array");
-		  },4000)
-	  })
-	  new Promise((resolve,reject)=>{
-		  setTimeout(()=>{
-			  resolve("fifth array");
-		  },5000)
-	  })
-]
+// script.js
 
-let x=Promise.any(promises);
-x.then((data)=>{
-	let output=document.getElementById("output");
-	output.innerText=data;
-})
+// Array of promises
+var promises = [
+    createPromise(1, 5),
+    createPromise(1, 5),
+    createPromise(1, 5),
+    createPromise(1, 5),
+    createPromise(1, 5)
+];
 
+// Utility function to create a promise with a random delay
+function createPromise(min, max) {
+    var delay = Math.random() * (max - min) + min;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(delay);
+        }, delay * 1000);
+    });
+}
 
-
-    // Print the result of the first resolved promise to the output div
-    
+// Use Promise.any() to wait for the first promise to resolve
+Promise.any(promises)
+    .then(result => {
+        // Print the result to the output div
+        var outputDiv = document.getElementById("output");
+        outputDiv.textContent = "First promise resolved with delay: " + result + " seconds";
+    })
+    .catch(error => {
+        console.error("All promises rejected:", error);
+    });
