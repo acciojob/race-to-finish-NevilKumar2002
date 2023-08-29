e1d15dcee37a3caf2f71cbf16d4ecf8ef34de7f5
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-29 09:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
  const promises = [
-      new Promise(resolve => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 4000 + 1000)),
-      new Promise(resolve => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 4000 + 1000)),
-      new Promise(resolve => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 4000 + 1000)),
-      new Promise(resolve => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 4000 + 1000)),
-      new Promise(resolve => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 4000 + 1000))
-    ];
+      new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("first array");
+		  },1000)
+	  })
+	  new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("second array");
+		  },2000)
+	  })
+	  new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("third array");
+		  },3000)
+	  })
+	 new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("fourth array");
+		  },4000)
+	  })
+	  new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("fifth array");
+		  },5000)
+	  })
+]
+
+let x=Promise.any(promises);
+x.then((data)=>{
+	let output=document.getElementById("output");
+	output.innerText=data;
+})
+
+
 
     // Print the result of the first resolved promise to the output div
-    Promise.any(promises)
-      .then(result => {
-        const outputDiv = document.getElementById('output');
-        outputDiv.textContent = result;
-      })
-      .catch(error => {
-        const outputDiv = document.getElementById('output');
-        outputDiv.textContent = "All promises failed: " + error.message;
-      });
+    
